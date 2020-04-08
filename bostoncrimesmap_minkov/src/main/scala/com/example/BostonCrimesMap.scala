bb8e9306428b62d4f499219c87775eeab0720ada
Миньков Сергей <Sergey.Minkov@inside.glowbyte.com>
2020-04-08 20:16:23 +0300

    v3

--- bostoncrimesmap_minkov/src/main/scala/com/example/BostonCrimesMap.scala
+++ bostoncrimesmap_minkov/src/main/scala/com/example/BostonCrimesMap.scala
@@ -2,11 +2,8 @@ package com.example
 
 object BostonCrimesMap extends App {
   import org.apache.spark.sql.SparkSession
-  import org.apache.spark.sql.functions.{broadcast, count, sum, split, trim, array}
-  import org.apache.spark.sql.expressions.MutableAggregationBuffer
-  import org.apache.spark.sql.expressions.UserDefinedAggregateFunction
-  import org.apache.spark.sql.Row
-  import org.apache.spark.sql.types._
+  import org.apache.spark.sql.functions.{broadcast, count, sum, split, trim, row_number, lead}
+  import org.apache.spark.sql.expressions.Window
 
   val spark = SparkSession.builder.appName("App").getOrCreate()
   import spark.implicits._
@@ -14,37 +11,6 @@ object BostonCrimesMap extends App {
   val crime = args(0)
   val codes = args(1)
   val path = args(2)
-
-  class ListAgg_ extends UserDefinedAggregateFunction {
-
-     def inputSchema: StructType =
-      StructType(StructField("name_cnt", ArrayType(StringType)) :: Nil)
-
-     def bufferSchema: StructType =
-      StructType(StructField("names_cnt", ArrayType(ArrayType(StringType))) :: Nil)
-
-     def dataType: DataType = StringType
-
-     def deterministic: Boolean = true
-
-     def initialize(buffer: MutableAggregationBuffer): Unit = {
-      buffer(0) = Seq.empty[Seq[String]]
-    }
-
-     def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
-      buffer(0) = buffer.getSeq(0) :+ input.getSeq(0)
-    }
-
-     def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
-      buffer1(0) = buffer1.getSeq(0) ++ buffer2.getSeq(0)
-    }
-
-     def evaluate(buffer: Row): Any = {
-      buffer.getSeq(0).sortWith((x: Seq[String], y: Seq[String]) => x(1).toInt > y(1).toInt).slice(0, 3).map((x: Seq[String]) => x.head).mkString(", ")
-    }
-  }
-
-  val listAgg = new ListAgg_()
 
   val crime_df = spark.read.option("header", "true").option("inferSchema", "true").csv(crime)
   val codes_df_raw = spark.read.option("header", "true").option("inferSchema", "true").csv(codes)
@@ -54,19 +20,19 @@ object BostonCrimesMap extends App {
     .join(broadcast(codes_df), $"offense_code" === $"code")
     .groupBy($"district", $"name")
     .count()
-    .withColumn("name_cnt", array($"name", $"count"))
+    .withColumn("rn", row_number.over(Window.partitionBy($"district").orderBy($"count".desc)))
+    .withColumn("name_1", lead($"name", 1).over(Window.partitionBy($"district").orderBy($"count".desc)))
+    .withColumn("name_2", lead($"name", 2).over(Window.partitionBy($"district").orderBy($"count".desc)))
     .createOrReplaceTempView("df_2_tmp")
-
-  spark.udf.register("listAgg", listAgg)
 
   val df_2 = spark.sql(
     "SELECT" +
     "  COALESCE(district, 'null') AS district" +
-    ", listAgg(name_cnt) AS frequent_crime_types " +
+    ", CONCAT_WS(', ', name, name_1, name_2) as frequent_crime_types " +
     "FROM" +
     "  df_2_tmp " +
-    "GROUP BY" +
-    "  district"
+    "WHERE" +
+    "  rn = 1"
   )
 
   crime_df.groupBy($"district", $"year", $"month").agg(
@@ -98,5 +64,4 @@ object BostonCrimesMap extends App {
     $"lat",
     $"lng"
   ).coalesce(1).write.mode("overwrite").parquet(path)
-
 }
